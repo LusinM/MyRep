2f51d6f8be3d1f04cdee4128cf04154277eb7297
Lusinemalkhasyan <lusy.malkhasyan95@gmail.com>
2020-09-14 22:04:55 +0400

    Update homework2.js

--- js/homework2.js
+++ js/homework2.js
@@ -1,11 +1,11 @@
 //step 1
-const PI =Math.PI
+const PI =Math.PI;
 // step 2
-let r = parseFloat(prompt("Enter the redius"))
-console.log(r)
+let r = parseFloat(prompt("Enter the redius"));
+console.log(r);
 //step 3
-let a = PI * r * r
-let ps= 2 * PI * r
+let a = PI * r * r;
+let ps= 2 * PI * r;
 // step 4
-alert('The area of circle is ' + a)
-alert('The perimetr of circle is '+ p)
+alert('The area of circle is ' + a);
+alert('The perimetr of circle is '+ p);
