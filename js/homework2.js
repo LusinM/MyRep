431718dc6536b79f677b5eeddd63679ca012a883
Lusinemalkhasyan <lusy.malkhasyan95@gmail.com>
2020-09-14 21:42:22 +0400

    Create homework2.js

---
+++ js/homework2.js
@@ -0,0 +1,11 @@
+//step 1
+const PI =Math.PI
+// step 2
+let r = parseFloat(prompt("Enter the redius"))
+console.log(r)
+//step 3
+let a = PI * r * r
+let ps= 2 * PI * r
+// step 4
+alert('The area of circle is ' + a)
+alert('The perimetr of circle is '+ p)
